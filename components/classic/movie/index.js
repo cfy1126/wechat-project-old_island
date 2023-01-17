2a9f29126be7d110d33d5233eca11662c5b0f2d8
Cao.F.Y <2017954256@qq.com>
2023-01-17 18:19:04 +0800

    master

--- components/classic/movie/index.js
+++ components/classic/movie/index.js
@@ -1,24 +1,24 @@
 // components/classic/movie/index.js
+import {classicBeh} from '../classic-beh.js'
 Component({
-    /**
-     * 组件的属性列表
-     */
-    properties: {
-        img: String,
-        content: String
-    },
+  /**
+   * 组件的属性列表
+   */
+  behaviors:[classicBeh],
 
-    /**
-     * 组件的初始数据
-     */
-    data: {
+  properties: {
+  },
 
-    },
+  /**
+   * 组件的初始数据
+   */
+  data: {
+  },
 
-    /**
-     * 组件的方法列表
-     */
-    methods: {
+  /**
+   * 组件的方法列表
+   */
+  methods: {
 
-    }
+  }
 })
--- models/like.js
+++ models/like.js
@@ -1,17 +1,29 @@
-import HTTP from '../util/http'
+import {
+    HTTP
+} from '../util/http.js'
 
 class LikeModel extends HTTP {
-    like(behavior, artID, category) {
-        let url = behavior === 'like' ? '/like' : '/like/cancel'
+    like(behavior, artID, category){
+        let url = behavior == 'like' ? 'like' : 'like/cancel'
         this.request({
-            url,
-            method:"POST",
-            data:{
-                art_id:artID,
-                type:category
+            url: url,
+            method: 'POST',
+            data: {
+                art_id: artID,
+                type: category
             }
         })
     }
+
+    getClassicLikeStatus(artID,category,sCallback){
+        this.request({
+            url:'classic/' + category + '/' + artID + '/favor',
+            success:sCallback
+        })
+    }
+
 }
 
-export default LikeModel
+export {
+    LikeModel
+}
